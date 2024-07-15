a03111523e9747623d9065466623e1845c380902
nirmalya2003 <rimaadak@gmail.com>
2024-07-15 21:54:46 +0530

    ok

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CookFood
+# CookFood-From Recipe to Reality
 
 I have nothing to say here, just work to build a dreamwork. We are halfway there.
-Now it is completed and published on 31/03/2024.
+Now it is completed and published on 15/07/2024.
--- addRecipe/comments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comments (
-    commentid INT AUTO_INCREMENT PRIMARY KEY,
-    recipeid INT NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    comment TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (recipeid) REFERENCES addrecipe(idRecipe)
-);
